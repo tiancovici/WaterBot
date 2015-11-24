2662bfb6791e40a65aaee989d3cb2b7305ff1388
mikemeding <mikeymeding@gmail.com>
2015-11-24 15:26:47 -0500

    added localizer that works. map messages next

---
+++ localizer_node/launch.sh
@@ -0,0 +1,2 @@
+source devel/setup.bash
+roslaunch uml_mcl mcl.launch
---
+++ localizer_node/src/no_weights/build/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/fredm/Dropbox/uml/teaching/91.451/2015/mcl_ws/src/no_weights/lib/libgtest_main.pdb"
+  "/home/fredm/Dropbox/uml/teaching/91.451/2015/mcl_ws/src/no_weights/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
